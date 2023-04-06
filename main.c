ececc560e2b906ffb62a581a9b421a1646ed04ed
Ysa <122658811+yeesaa@users.noreply.github.com>
2023-04-07 01:20:18 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <boolean.h>
+
+
+
+int main()
+{
+	A[3] = {1,2,3};
+	S[9][2] = {{1,1},{1,2},{1,3},{2,1},{2,2},{2,3},{3,1},{3,2},{3,3}};
+	V[2] = {true, false}
+
+	return 0;
+}
